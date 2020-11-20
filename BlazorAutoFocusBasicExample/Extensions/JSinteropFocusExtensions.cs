6695714a7706c905d9387f04a979f74be73112f9
q7314568 <q7314568@gmail.com>
2020-11-20 22:26:54 +0800

    第一次專案推送

--- BlazorAutoFocusBasicExample/Extensions/JSinteropFocusExtensions.cs
+++ BlazorAutoFocusBasicExample/Extensions/JSinteropFocusExtensions.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace BlazorAutoFocusBasicExample.Extensions
 {
+    /// <summary>
+    /// 利用myScript.js裡面寫的DoFocus方法，來實現利用JSRuntime達到focus特定元件的方法
+    /// </summary>
     public static class JSinteropFocusExtensions
     {
         public static ValueTask DoFocusAsync(this IJSRuntime jSRuntime,ElementReference elementReference)
